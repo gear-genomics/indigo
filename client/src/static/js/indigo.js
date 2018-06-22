e508f7f0c91d7ec4f6f36a0b7517a72b70f71f40
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2018-06-22 16:44:37 +0200

    wip client redesign

---
+++ client/src/static/js/indigo.js
@@ -0,0 +1,53 @@
+$('#mainTab a').on('click', function(e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+const resultLink = document.getElementById('link-results')
+
+const submitButton = document.getElementById('btn-submit')
+submitButton.addEventListener('click', function() {
+  resultLink.click()
+  run()
+})
+
+const exampleButton = document.getElementById('btn-example')
+// exampleButton.addEventListener('click', loadExample)
+
+const inputFile = document.getElementById('inputFile')
+const targetFastaFile = document.getElementById('targetFileFasta')
+const targetChromatogramFile = document.getElementById('targetFileChromatogram')
+const targetGenomes = document.getElementById('target-genome')
+const targetTabs = document.getElementById('target-tabs')
+
+function run() {
+  const formData = new FormData()
+
+  if (inputFile.files.length === 0) {
+    // TODO error message
+    return
+  }
+
+  formData.append('queryFile', inputFile.files[0])
+
+  const target = targetTabs.querySelector('a.active').id
+
+  if (target.startsWith('target-genome')) {
+    const genome = targetGenomes.querySelector('option[selected]').textContent
+    formData.append('genome', genome)
+  } else if (target.startsWith('target-fasta')) {
+    if (targetFastaFile.files.length === 0) {
+      // TODO error message
+      return
+    }
+    formData.append('fastaFile', targetFastaFile.files[0])
+  } else if (target.startsWith('target-chromatogram')) {
+    if (targetChromatogramFile.files.length === 0) {
+      // TODO error message
+      return
+    }
+    formData.append('chromatogramFile', targetChromatogramFile.files[0])
+  }
+
+  console.log('Submitting', formData)
+}
